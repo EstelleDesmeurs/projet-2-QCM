aae51152c372c141487da16c3f4d1a5f7fe0239a
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-05-15 15:24:46 +0200

    1

---
+++ resultat.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+//boucle >> condition dans la boucle pour rajouter +1 à chaque fois que c'est correct'. Puis, résultat?
+
+
+
+/*
+function validate($y) {
+    $y= filter_var($y, FILTER_VALIDATE_STRING);
+    return $y;
+}
+                                         
+$firstName = validate($_POST['firstName']);
+$lastName = validate($_POST['lastName']);                            
+$message = validate($_POST['message']);
+*/
+?>
