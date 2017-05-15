a1e40eec2343d7e8005912c68764fe9004602ef8
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-05-15 16:44:39 +0200

    début

--- resultat.php
+++ resultat.php
@@ -1,12 +1,39 @@
 <?php
 
+$bonneReponse = 0;
+//print_r($_POST);
 
 
-//boucle >> condition dans la boucle pour rajouter +1 à chaque fois que c'est correct'. Puis, résultat?
-
-
-
+if ($_POST['R1']=='T1.B') {
+    $bonneReponse++ ;
+}
+if ($_POST['R2']=='T2.A') {
+    ++$bonneReponse ;
+}
+if ($_POST['R3']=='T3.C') {
+    ++$bonneReponse ;
+}
+print_r($_POST);
+echo $_POST;
+echo "Vous avez obtenu ".$bonneReponse." bonnes réponses.";
+*/
 /*
+$bonneReponse= array (
+                R1 => 'T1.B',
+                R2 => 'T2.A',
+                R3 => 'T3.C'
+)
+$_POST= array(
+            R1 => $_POST['R1'],
+            R2 => $_POST['R2'],
+            R3 => $_POST['R3']
+)
+    
+$result = array_merge ($_POST, $bonneReponse);
+print_r($result);
+
+
+
 function validate($y) {
     $y= filter_var($y, FILTER_VALIDATE_STRING);
     return $y;
